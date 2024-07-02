8c7ab0831b2d567cd4dba6e11e6db76d796906ea
Ladoro <ounissi.karim83@gmail.com>
2024-07-03 01:09:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,57 @@
-// Jeu de Temps de Réaction
+// Variables globales
+let username;
+let userData = {};
+
+// Sélectionner les éléments HTML
+let userForm = document.getElementById('userForm');
+let usernameInput = document.getElementById('username');
+let startButton = document.getElementById('startButton');
+let reactionTimeGame = document.getElementById('reactionTimeGame');
+let targetClickGame = document.getElementById('targetClickGame');
 let reactionBox = document.getElementById('reactionBox');
 let reactionMessage = document.getElementById('reactionMessage');
+let startTargetGameButton = document.getElementById('startTargetGame');
+let targetGameArea = document.getElementById('targetGameArea');
+let targetGameMessage = document.getElementById('targetGameMessage');
+let reactionResults = document.getElementById('reactionResults');
+let targetResults = document.getElementById('targetResults');
+let resultsSection = document.getElementById('results');
+
+startButton.addEventListener('click', function() {
+    username = usernameInput.value;
+    if (username) {
+        userForm.style.display = 'none';
+        reactionTimeGame.style.display = 'block';
+        targetClickGame.style.display = 'block';
+        resultsSection.style.display = 'block';
+        loadUserData();
+        startReactionGame();
+    }
+});
+
+function loadUserData() {
+    let storedData = localStorage.getItem(username);
+    if (storedData) {
+        userData = JSON.parse(storedData);
+        displayResults();
+    } else {
+        userData = {
+            reactionTimes: [],
+            targetTimes: []
+        };
+    }
+}
+
+function saveUserData() {
+    localStorage.setItem(username, JSON.stringify(userData));
+}
+
+function displayResults() {
+    reactionResults.innerHTML = userData.reactionTimes.map(time => `<li>${time} secondes</li>`).join('');
+    targetResults.innerHTML = userData.targetTimes.map(time => `<li>${time} secondes</li>`).join('');
+}
+
+// Jeu de Temps de Réaction
 let startTime;
 
 reactionBox.addEventListener('click', function() {
@@ -8,6 +59,9 @@ reactionBox.addEventListener('click', function() {
         let endTime = new Date().getTime();
         let reactionTime = (endTime - startTime) / 1000;
         reactionMessage.textContent = `Temps de réaction : ${reactionTime} secondes`;
+        userData.reactionTimes.push(reactionTime);
+        saveUserData();
+        displayResults();
         reactionBox.style.backgroundColor = 'red';
         setTimeout(startReactionGame, Math.random() * 2000 + 1000);
     }
@@ -21,12 +75,7 @@ function startReactionGame() {
     }, Math.random() * 2000 + 1000);
 }
 
-startReactionGame();
-
 // Jeu de Clic sur Cible
-let startTargetGameButton = document.getElementById('startTargetGame');
-let targetGameArea = document.getElementById('targetGameArea');
-let targetGameMessage = document.getElementById('targetGameMessage');
 let targetCount = 0;
 let startTimeTargetGame;
 let totalTargets = 30;
@@ -46,6 +95,9 @@ function createTarget() {
         let endTime = new Date().getTime();
         let totalTime = (endTime - startTimeTargetGame) / 1000;
         targetGameMessage.textContent = `Temps total : ${totalTime} secondes`;
+        userData.targetTimes.push(totalTime);
+        saveUserData();
+        displayResults();
         return;
     }
     
